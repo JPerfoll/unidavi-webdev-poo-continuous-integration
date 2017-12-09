ee92aab8990224f01af1e22864c8cba2c3755265
Jean Perfoll <31888@UNIDAVI.local>
2017-12-09 08:45:49 -0200

    commit com a classe de testes automatizadas compilando mas com todos os testes ignorados

---
+++ src/main/java/webdev/poo/unidavi/webdev/poo/WordProblemSolver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webdev.poo.unidavi.webdev.poo;
+
+/**
+ *
+ * @author 31888
+ */
+public class WordProblemSolver {
+
+    public Object solve(String what_is_1_plus_1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
